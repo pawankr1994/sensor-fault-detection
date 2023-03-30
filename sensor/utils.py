c07e2c32e34b677629878e7f9da9a71a560df616
pawan kumar <pawanaryan93@gmail.com>
2023-03-30 21:01:14 +0530

    updated remaining data_transformation and model_trainer

--- sensor/utils.py
+++ sensor/utils.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from sensor.config import mongo_client
-from sensor.exception import SensorException
 from sensor.logger import logging
-import yaml
+from sensor.exception import SensorException
+from sensor.config import mongo_client
 import os,sys
+import yaml
 import numpy as np
 import dill
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
-
     """
     Description: This function return collection as dataframe
     =========================================================
@@ -18,7 +17,6 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
     =========================================================
     return Pandas dataframe of a collection
     """
-    
     try:
         logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
@@ -30,6 +28,7 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise SensorException(e, sys)
+    
 
 def write_yaml_file(file_path,data:dict):
     try:
@@ -40,7 +39,6 @@ def write_yaml_file(file_path,data:dict):
     except Exception as e:
         raise SensorException(e, sys)
 
-
 def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
     try:
         for column in df.columns:
@@ -49,3 +47,50 @@ def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
         return df
     except Exception as e:
         raise e
+
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of utils")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Exited the save_object method of utils")
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
